8fb0f93173f2421a78fbeea839686b7476fd21b1
Dimitar Dimitrov <42871796+Whimfoome@users.noreply.github.com>
2023-07-04 15:59:59 +0300

    fetch countries from api

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,55 +6,52 @@ import Dropdown from "react-bootstrap/Dropdown";
 import countriesData from "./countries.json"
 import { Form } from "react-bootstrap";
 
+interface CountriesApi {
+  continents: {
+    name: string;
+    countries: string[];
+  }[];
+}
+
+interface ContinentsApi {
+  continents: {
+    name: string;
+    info: string;
+  }[];
+}
+
 export default function Home() {
-  const [countries, setCountries] = useState([] as { name: string; type: string; }[]);
-  const [selected, setSelected] = useState({} as { name: string; type: string });
+  const [countries, setCountries] = useState({} as CountriesApi);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [info, setInfo] = useState("");
+  const [selected, setSelected] = useState({} as { name: string; continent: string });
 
   useEffect(() => {
     const getCountries = async () => {
-      // fetch("http://localhost:8080/countries")
-      //   .then(response => {
-      //     console.log('Response: ', response.status);
-      //     console.log(response.json());
-      //   })
-      //   .catch(err => {
-      //     console.error(err);
-      //   })
       try {
         const response = await fetch("http://localhost:8080/countries");
 
         if (!response.ok) {
           console.log(response);
-          throw new Error(`Error, status: ${response.status}`);
+          setErrorMessage("Couldn't fetch countries, please refresh page");
+          return;
         }
 
-        const result = await response.json();
+        const result: CountriesApi = await response.json();
+        setCountries(result);
         console.log(result);
-        // return result;
       } catch (err) {
         console.error(err);
       }
     }
 
-
     getCountries();
-
-    // Sort array alphabetically
-    // countriesData.sort(function (a, b) {
-    //   if (a.name < b.name) {
-    //     return -1;
-    //   }
-    //   if (a.name > b.name) {
-    //     return 1;
-    //   }
-    //   return 0;
-    // });
-
-    // setCountries(countriesData);
   }, [])
 
-  function handleSelection(country: { name: string; type: string }) {
+  function handleSelection(country: { name: string; continent: string }) {
     setSelected(country);
+
+    setInfo(country.continent);
   }
 
   return (
@@ -68,28 +65,34 @@ export default function Home() {
               </Dropdown.Toggle>
 
               <Dropdown.Menu as={CustomMenu}>
-                {countries.map((country) => {
-                  return (
-                    <Dropdown.Item key={crypto.randomUUID()} onClick={() => handleSelection(country)}>
-                      {country.name}
-                    </Dropdown.Item>
-                  );
+                {countries.continents && countries.continents.map((continent) => {
+                  return continent.countries.map((country) => {
+                    return (
+                      <Dropdown.Item key={crypto.randomUUID()} onClick={() => handleSelection({ name: country, continent: continent.name })}>
+                        {country}
+                      </Dropdown.Item>
+                    );
+                  })
                 })}
               </Dropdown.Menu>
             </Dropdown>
             <div>
               {
                 (() => {
-                  switch (selected.type) {
-                    case ('A'):
-                      return (<p>The employee should provide notarised copies of all official original documents (i.e. diploma, criminal record, etc.). The notarised copy should be in original (no copies of the notarised copy will be accepted). You need a notarisation of the sworn translator’s signature.</p>)
-                    case ('B'):
-                      return (<p>The employee should provide all official documents with an apostille</p>)
-                    case ('C'):
-                      return (<p>The employee needs to attest their official documents by the local Ministry of Foreign Affairs and then have it stamped in the Bulgarian embassy in their country of origin.</p>)
-                    default:
-                      return (<p></p>)
+                  if (info.length !== 0) {
+                    return <p>{info}</p>
                   }
+                  return <p>{errorMessage.length !== 0 ? errorMessage : ""}</p>
+                  // switch (selected.type) {
+                  //   case ('A'):
+                  //     return (<p>The employee should provide notarised copies of all official original documents (i.e. diploma, criminal record, etc.). The notarised copy should be in original (no copies of the notarised copy will be accepted). You need a notarisation of the sworn translator’s signature.</p>)
+                  //   case ('B'):
+                  //     return (<p>The employee should provide all official documents with an apostille</p>)
+                  //   case ('C'):
+                  //     return (<p>The employee needs to attest their official documents by the local Ministry of Foreign Affairs and then have it stamped in the Bulgarian embassy in their country of origin.</p>)
+                  //   default:
+                  //     return (<p></p>)
+                  // }
                 })()
               }
             </div>
