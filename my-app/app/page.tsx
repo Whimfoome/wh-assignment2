7c67103d47e8d7e28a67f8b253e4b58150e483ca
Dimitar Dimitrov <42871796+Whimfoome@users.noreply.github.com>
2023-07-04 12:47:50 +0300

    restore page from assignment 1 fully typed

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,95 +1,144 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import { useState, useEffect, forwardRef, Children, CSSProperties, ReactNode, isValidElement } from "react";
+import Container from "react-bootstrap/Container";
+import Dropdown from "react-bootstrap/Dropdown";
+import countriesData from "./countries.json"
+import { Form } from "react-bootstrap";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [countries, setCountries] = useState([] as { name: string; type: string; }[]);
+  const [selected, setSelected] = useState({} as { name: string; type: string });
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  useEffect(() => {
+    const getCountries = async () => {
+      // fetch("http://localhost:8080/countries")
+      //   .then(response => {
+      //     console.log('Response: ', response.status);
+      //     console.log(response.json());
+      //   })
+      //   .catch(err => {
+      //     console.error(err);
+      //   })
+      try {
+        const response = await fetch("http://localhost:8080/countries");
+
+        if (!response.ok) {
+          console.log(response);
+          throw new Error(`Error, status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+        // return result;
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+
+    getCountries();
+
+    // Sort array alphabetically
+    // countriesData.sort(function (a, b) {
+    //   if (a.name < b.name) {
+    //     return -1;
+    //   }
+    //   if (a.name > b.name) {
+    //     return 1;
+    //   }
+    //   return 0;
+    // });
+
+    // setCountries(countriesData);
+  }, [])
+
+  function handleSelection(country: { name: string; type: string }) {
+    setSelected(country);
+  }
+
+  return (
+    <div className="App">
+      <Container>
+        <div className="wrapper">
+          <div className="content">
+            <Dropdown className="dropdown-button">
+              <Dropdown.Toggle variant="custom">
+                {Object.keys(selected).length === 0 ? 'Select Country' : selected.name}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu as={CustomMenu}>
+                {countries.map((country) => {
+                  return (
+                    <Dropdown.Item key={crypto.randomUUID()} onClick={() => handleSelection(country)}>
+                      {country.name}
+                    </Dropdown.Item>
+                  );
+                })}
+              </Dropdown.Menu>
+            </Dropdown>
+            <div>
+              {
+                (() => {
+                  switch (selected.type) {
+                    case ('A'):
+                      return (<p>The employee should provide notarised copies of all official original documents (i.e. diploma, criminal record, etc.). The notarised copy should be in original (no copies of the notarised copy will be accepted). You need a notarisation of the sworn translator’s signature.</p>)
+                    case ('B'):
+                      return (<p>The employee should provide all official documents with an apostille</p>)
+                    case ('C'):
+                      return (<p>The employee needs to attest their official documents by the local Ministry of Foreign Affairs and then have it stamped in the Bulgarian embassy in their country of origin.</p>)
+                    default:
+                      return (<p></p>)
+                  }
+                })()
+              }
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div >
+  );
+}
+
+interface Props {
+  children?: ReactNode;
+  style: CSSProperties;
+  className: string;
+  'aria-labelledby': string;
+}
+
+const CustomMenu = forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const { children, style, className, 'aria-labelledby': labeledBy } = props;
+  const [value, setValue] = useState('');
+
+  return (
+    <div
+      ref={ref}
+      style={style}
+      className={className}
+      aria-labelledby={labeledBy}
+    >
+      <div className="m-2 p-0">
+        <Form.Control
+          autoFocus
+          placeholder="Type to filter..."
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
         />
       </div>
+      <div className="custom-dropdown">
+        {Children.toArray(children).filter(
+          (child) => {
+            if (isValidElement(child)) {
+              const childText = child.props.children as string;
+              return !value || childText.toLowerCase().startsWith(value);
+            }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            return false;
+          }
+        )}
       </div>
-    </main>
-  )
-}
+    </div>
+  );
+});
+CustomMenu.displayName = "CustomMenu";
